b0ab99d904385064124f9973e991787d91ff670d
mposolda <mposolda@gmail.com>
2024-05-17 17:50:24 +0200

    Reading from config finished
    Signed-off-by: mposolda <mposolda@gmail.com>

--- mather/config.js
+++ mather/config.js
@@ -1,16 +1,47 @@
 class Config {
   #examplesCount = 2;
+  #exampleType;
 
   getExamplesCount() {
     return this.#examplesCount;
+  }
+
+  setExamplesCount(examplesCount) {
+    this.#examplesCount = examplesCount;
+  }
+
+  getExampleType() {
+    return this.#exampleType;
+  }
+
+  setExampleType(exampleType) {
+    this.#exampleType = exampleType;
+  }
+
+  toStringg() {
+    return "examplesCount=" + this.#examplesCount + ", exampleType=" + this.#exampleType;
   }
 }
 
 class ConfigProvider {
 
   confirmConfig() {
-    console.log("Configuration confirmed");
+    var exampleType = this.#getSelectedExampleType();
+
+    config.setExampleType(exampleType);
+
+    console.log("Configuration confirmed: " + config.toStringg());
     mather.startMe();
+  }
+
+  #getSelectedExampleType() {
+    var inputs = document.getElementsByName('examplesType')
+    for (var i = 0; i < inputs.length; i++) {
+      if (inputs[i].checked) {
+        var selected = inputs[i];
+        return selected.id;
+      }
+    }
   }
 
 }
